ced06a7fe0b1ccd74e019101cbb4ece52ec44782
Yerzhan <yerzhan.taikeshev@gmail.com>
2021-02-03 23:36:34 +0600

    section carousel

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+// const num = 50;
+// if (num > 40) {
+//     console.log('верно')
+// } else {
+//     console.log('неправильно')
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(i);
+// }
+
+// function get(a, b) {
+//     console.log(a + b)
+// }
+// get(2, 3);
+
+// let f = 2;
+// let g = 4;
+// console.log(f + g)
