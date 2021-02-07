dee4e4e0ffda8f21dfaf918029f4026d003a3383
Yerzhan <yerzhan.taikeshev@gmail.com>
2021-02-07 22:51:01 +0600

    form

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,13 @@
+// document Pulse
 $(document).ready(function () {
+    // slaider
     $('.carousel__inner').slick({
         speed: 1000,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
     });
+    // tab
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
         $(this)
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
@@ -21,4 +24,49 @@ $(document).ready(function () {
     };
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+    // modal windows
+    $('[data-modal="consultation"]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+    $('.button_catalog').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+    // validate form
+    function validateForm(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите не меньше {0} символов!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен аресс почты"
+                }
+            }
+        });
+    };
+    validateForm('#consultation-form');
+    validateForm('#consultation form');
+    validateForm('#order form');
+    // masked tel form
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 });
