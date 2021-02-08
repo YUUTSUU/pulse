fe6b436498f9a35f08e19d1e5b479a486d1b7bdd
Yerzhan <yerzhan.taikeshev@gmail.com>
2021-02-08 23:45:16 +0600

    animated

--- src/js/script.js
+++ src/js/script.js
@@ -13,6 +13,7 @@ $(document).ready(function () {
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
+    // tab link
     function toggleSlide(a) {
         $(a).each(function (i) {
             $(this).on('click', function (e) {
@@ -69,4 +70,33 @@ $(document).ready(function () {
     validateForm('#order form');
     // masked tel form
     $('input[name=phone]').mask("+7 (999) 999-99-99");
+    // PHP mail
+    $('form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+    // smooth scroll
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1145) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+    $("a[href^='#']").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
+    // 
 });
