0f6c797b1c6de49faae5e8afccfe5c943af70b99
Yerzhan <yerzhan.taikeshev@gmail.com>
2021-02-09 22:48:49 +0600

    gulp

--- src/js/script.js
+++ src/js/script.js
@@ -93,10 +93,11 @@ $(document).ready(function () {
             $('.pageup').fadeOut();
         }
     });
-    $("a[href^='#']").click(function () {
+    $("a[href='#up']").click(function () {
         const _href = $(this).attr("href");
         $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
         return false;
     });
-    // 
+    // animated
+    new WOW().init();
 });
