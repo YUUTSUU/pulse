7271c6f314219ec751c6895a0ad2b24db237fe6d
Yerzhan <yerzhan.taikeshev@gmail.com>
2021-02-10 16:32:20 +0600

    hosting

--- dist/js/script.js
+++ dist/js/script.js
@@ -7,13 +7,13 @@ $(document).ready(function () {
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
     });
-    // tab menu
+    // tab
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
         $(this)
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
             .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
     });
-    // tab cart
+    // tab link
     function toggleSlide(a) {
         $(a).each(function (i) {
             $(this).on('click', function (e) {
